fa151baad8312ecc61313c71f332df97f3a1cfc0
sojjyCodes <ebenezeralagbe26@gmail.com>
2021-01-08 03:33:02 +0100

    Successfully migrated and created a Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/register', [RegisterController::class, 'index']);
 
 Route::get('/posts', function () {
     return view('posts.index');
