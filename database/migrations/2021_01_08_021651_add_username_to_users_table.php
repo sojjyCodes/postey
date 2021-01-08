7c18b1ff6aff0f3ad5ee9a964603b5eec4dc09cd
sojjyCodes <ebenezeralagbe26@gmail.com>
2021-01-08 05:20:15 +0100

    Updates

--- database/migrations/2021_01_08_021651_add_username_to_users_table.php
+++ database/migrations/2021_01_08_021651_add_username_to_users_table.php
@@ -14,7 +14,7 @@ class AddUsernameToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username');
+            //
         });
     }
 
@@ -26,7 +26,7 @@ class AddUsernameToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('username');
+            // $table->dropColumn('');
         });
     }
 }
