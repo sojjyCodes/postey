57be633fd7a6f294ad724edc86cc0d02af45ee22
sojjyCodes <ebenezeralagbe26@gmail.com>
2021-02-14 01:34:50 +0100

    Updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,7 +22,7 @@
 
                 <div class="mb-4">
                     <label for="password" class="sr-only">Name</label>
-                    <input type="password" name="password" id="password" placeholder="Choose a password" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror" value="">
+                    <input type="password" name="password" id="password" placeholder="Enter your password" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror" value="">
                     @error('password')
                     <div class="text-red-500 mt-2 text-sm">
                         {{ $message }}
